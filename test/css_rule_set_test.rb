b6c6a3f8a0abf606f4f0e5e5999971c0e68a7b6e
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-17 11:06:27 +1200

    added more tests to cover box model hack

--- test/css_rule_set_test.rb
+++ test/css_rule_set_test.rb
@@ -27,4 +27,36 @@ class CssRuleSetTest < Test::Unit::TestCase
 		assert_equal(expected, rs.to_hash)
 	end
 
+	def test_rule_set_really_long_with_spaces
+		css = <<-CSS
+			background-color: #123abc;
+			margin : 20px ;
+			padding: 10px  5px  3px 8px ;
+		 	width: 100px;
+		CSS
+		rs = CssToolkit::RuleSet.new({:selector => 'body', :declarations => css})
+
+		expected = {['body']=>['background-color:#123abc','margin:20px', 'padding:10px 5px 3px 8px','width:100px']}
+		assert_equal(expected, rs.to_hash)
+	end
+
+	def test_rule_set_really_long_with_spaces_and_box_model_hack
+		# NB hack is actually:
+		# voice-family: "\"}\"";
+		# extra escaped are for Ruby
+		css = <<-CSS
+			background-color: #123abc;
+			margin : 20px ;
+			padding: 10px  5px  3px 8px ;
+			width: 100px;
+			voice-family: "\\"}\\"";
+			voice-family:inherit;
+			width: 200px;
+		CSS
+		rs = CssToolkit::RuleSet.new({:selector => 'body', :declarations => css})
+
+		expected = {['body']=>['background-color:#123abc','margin:20px', 'padding:10px 5px 3px 8px','width:100px','voice-family:"\\"}\\""','voice-family:inherit','width:200px']}
+		assert_equal(expected, rs.to_hash)
+	end
+
 end
