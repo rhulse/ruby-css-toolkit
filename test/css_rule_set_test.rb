daa7f1ea12efada900c77d56358a7660876385e4
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-17 16:31:50 +1200

    added charset class to hold character encoding

--- test/css_rule_set_test.rb
+++ test/css_rule_set_test.rb
@@ -119,5 +119,18 @@ class CssRuleSetTest < Test::Unit::TestCase
 		assert_equal(expected, sheet.to_s(:multi_line))
 	end
 
+	def test_charset
+		charset = CssToolkit::Charset.new
+		expected = '@charset "UTF-8";'
+		assert_equal(expected, charset.to_s)
+
+		charset << 'UTF-16'
+		expected = '@charset "UTF-16";'
+		assert_equal(expected, charset.to_s)
+
+		charset.encoding = 'UTF-32'
+		expected = '@charset "UTF-32";'
+		assert_equal(expected, charset.to_s)
+	end
 
 end
