5755b59f419321c63d22638c72931d7fa843e6a2
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-26 09:21:05 +1200

    add more declation tests
    This tests the declaration object directly. The same
    tests are now applied here, and on the whole process.

--- test/css_declaration_test.rb
+++ test/css_declaration_test.rb
@@ -37,4 +37,88 @@ class CssDeclarationTest < Test::Unit::TestCase
 		assert_equal(expected, declaration.to_s)
 	end
 
+	def test_decimals
+		declaration = CssToolkit::Declaration.new('margin', '0.6px 0.333pt 1.2em 8.8cm')
+		expected = "margin:.6px .333pt 1.2em 8.8cm"
+		declaration.optimize_zeros
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_rgb_colors
+		declaration = CssToolkit::Declaration.new('color', 'rgb(123, 123, 123)')
+		expected = "color:#7b7b7b"
+		declaration.optimize_colors
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_repeating_colors
+		declaration = CssToolkit::Declaration.new('color', '#ffeedd')
+		expected = "color:#fed"
+		declaration.optimize_colors
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_filter_untouched
+		declaration = CssToolkit::Declaration.new('filter', 'chroma(color="#FFFFFF")')
+		expected = 'filter:chroma(color="#FFFFFF")'
+		declaration.optimize_colors
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_rgb_in_background
+		declaration = CssToolkit::Declaration.new('background', 'none repeat scroll 0 0 rgb(255, 0,0)')
+		expected = 'background:none repeat scroll 0 0 #f00'
+		declaration.optimize_colors
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_rgba_is_untouched
+		declaration = CssToolkit::Declaration.new('alpha', 'rgba(1, 2, 3, 4)')
+		expected = 'alpha:rgba(1, 2, 3, 4)'
+		declaration.optimize_colors
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_optimize_zeros_1
+		declaration = CssToolkit::Declaration.new('margin', '0px 0pt 0em 0%')
+		expected = 'margin:0'
+		declaration.optimize_zeros
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_optimize_zeros_2
+		declaration = CssToolkit::Declaration.new('_padding-top', '0ex')
+		expected = '_padding-top:0'
+		declaration.optimize_zeros
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_optimize_zeros_3
+		declaration = CssToolkit::Declaration.new('_padding-top', '0ex')
+		expected = '_padding-top:0'
+		declaration.optimize_zeros
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_optimize_zeros_4
+		declaration = CssToolkit::Declaration.new('background-position', '0 0')
+		expected = 'background-position:0 0'
+		declaration.optimize_zeros
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_optimize_zeros_5
+		declaration = CssToolkit::Declaration.new('padding', '0in 0cm 0mm 0pc')
+		expected = 'padding:0'
+		declaration.optimize_zeros
+		assert_equal(expected, declaration.to_s)
+	end
+
+	def test_downcase_property
+		declaration = CssToolkit::Declaration.new('MaRGIn', '5px')
+		declaration.downcase_property
+		expected = 'margin:5px'
+		assert_equal(expected, declaration.to_s)
+	end
+
 end
