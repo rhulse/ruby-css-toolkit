98923582f608cc5c6019abc27bb7b88fa7ae5b03
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-25 17:26:00 +1200

    added YUI test suite to CSS Tidy
    Now to make them all pass...

--- test/css_tidy_tests.rb
+++ test/css_tidy_tests.rb
@@ -27,4 +27,16 @@ class CssTidyTest < Test::Unit::TestCase
 		assert_equal(expected_css, resultant_css)
 	end
 
+	# build our YUI tests based on the available files
+	# the tests are run as per normal
+	test_files = Dir.glob(File.join(File.dirname(__FILE__), 'yuicss/*.css'))
+	test_files.each_with_index do |file, idx|
+  	test_css 			= File.read(file)
+  	expected_css 	= File.read(file + '.min')
+		test_name = File.basename(file, ".css")
+		define_method("test_yui_css_#{test_name}") do
+			test_name = File.basename(file, ".css")
+			assert_equal(expected_css, @tidy.tidy(test_css), "YUI test #{test_name} failed")
+		end
+	end
 end
