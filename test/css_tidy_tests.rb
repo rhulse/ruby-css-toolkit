d5e656996372c7af1037efd6f3423ddebc780088
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-25 16:36:37 +1200

    first test for tidy method

---
+++ test/css_tidy_tests.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+# Test cases for preping a file for parsing
+class CssTidyTest < Test::Unit::TestCase
+
+	def setup
+		@tidy = CssTidy::Tidy.new()
+	end
+
+	def test_basic
+		css = <<-CSS
+		body {
+			background: #D6BFDC url('http://this.is-a.test/path/to/image.gif') repeat-x;
+			padding: 0;
+			margin: 0;
+			color: #727272;
+		}
+
+		p {
+			margin:0;
+			hack: "\\"}";
+			margin:10px ;
+		}
+		CSS
+		expected_css = %q[body{background:#D6BFDC url('http://this.is-a.test/path/to/image.gif') repeat-x;padding:0;margin:0;color:#727272}p{margin:0;hack:"\\"}";margin:10px}]
+		resultant_css = @tidy.tidy(css)
+		assert_equal(expected_css, resultant_css)
+	end
+
+end
