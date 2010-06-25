62d33d4be8f659dfded9e3e6878cb70892f4a686
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-25 23:30:42 +1200

    added code to skip certain comments
    Passed YUI tests bug2527974.css, preserve-strings.css,
    and preserve-string.css

--- lib/modules/css_stylesheet.rb
+++ lib/modules/css_stylesheet.rb
@@ -23,7 +23,22 @@ module CssToolkit
 		end
 
 		def optimize
-			@nodes.each {|node| node.optimize}
+			@nodes.each_with_index do |node, idx|
+				if node.class == CssToolkit::Comment
+					if node.is_special?
+						next # do nothing
+					elsif node.is_ie5_hack?
+						node.text = '\\'  # replace it
+						keep_next_comment = true
+					elsif keep_next_comment
+						node.text = ''  # replace it
+						keep_next_comment = false
+					else
+						node.printable = false # don't print this one
+					end
+				end
+				node.optimize
+			end
 		end
 
 	end
