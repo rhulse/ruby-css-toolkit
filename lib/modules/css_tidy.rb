8493fcff07e9cead173fb606e965f5cfe2c5b5a9
Richard Hulse <richard.hulse@radionz.co.nz>
2010-06-27 14:32:51 +1200

    added size display to command line tidy

--- lib/modules/css_tidy.rb
+++ lib/modules/css_tidy.rb
@@ -2,19 +2,23 @@
 
 module CssTidy
 	class Tidy
+		attr_reader :input_size, :output_size
 
 		def initialize
 			@parser = Parser.new
 		end
 
 		def tidy(css, format=:one_line)
+			@input_size = css.length
 			options = {
 				:downcase_selectors => true,
 				:downcase_properties => true
 			}
 			@stylesheet = @parser.parse(css)
 			optimize(options)
-			@stylesheet.to_s(format)
+			compressed_css = @stylesheet.to_s(format)
+			@output_size = compressed_css.length
+			compressed_css
 		end
 
 		def optimize(options)
