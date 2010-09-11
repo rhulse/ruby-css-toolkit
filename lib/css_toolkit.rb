4d8fcf952aaa3d05b8ef5cd6e0b65cfa47b04930
Richard Hulse <richard.hulse@radionz.co.nz>
2010-09-11 14:07:49 +1200

    Add tidy function, add default line length, bump version

--- lib/css_toolkit.rb
+++ lib/css_toolkit.rb
@@ -6,10 +6,16 @@ require 'modules/css_tidy'
 
 module CssToolkit
 	include YuiCompressor
+  VERSION = "1.3"
 
-	def yui_compressor(css, line_length)
+	def yui_compressor(css, line_length=0)
 		yui = Yui.new()
 		yui.compress(css, line_length)
+	end
+
+	def css_tidy(css, opts={})
+		tidy = Tidy.new()
+		tidy.tidy(css, line_length)
 	end
 
 end
